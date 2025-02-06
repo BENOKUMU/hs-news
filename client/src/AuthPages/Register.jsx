5567badf269651f6982ddae33e3617b0e895cdb8
Benjamin Okumu <benjaminokumu12@gmail.com>
2025-02-06 08:46:59 +0300

    Update

--- client/src/AuthPages/Register.jsx
+++ client/src/AuthPages/Register.jsx
@@ -1,50 +1,104 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import { Eye, EyeOff } from "react-feather";
+import AuthFooter from "./AuthFooter";
 
 const Register = () => {
+  const [email, setEmail] = useState("")
+  const [ showPassword, setShowPassword ] = useState("");
+
   const handleSubmit = (e) => {
     console.log(e);
   };
-  
+
   const handleEmailChange = (e) => {
     console.log(e);
   };
 
-
   return (
-    <div className="flex items-start pt-8 pb-10 justify-center bg-red-100">
-      <div className="max-w-lg w-full p-10 bg-white rounded-xl shadow-lg">
-        <div className="flex flex-col items-center">
-          <img src="/images/Header/logo.png" alt="" className="w-auto h-12" />
-          <h2 className="mt-6 text-center text-2xl font-extrabold text-gray-900">
-            Create Your CNN Account
-          </h2>
-          <div className="text-sm mt-2">
-            <Link
-              to="/login"
-              className="font-medium text-black hover:text-gray-700"
-            >
-              Already have an account? <span>Sign In</span>
-            </Link>
-          </div>
-        </div>
-        <form className="mt-8" onSubmit={handleSubmit}>
-          <div>
-            <div className="mb-4">
-              <label htmlFor=""></label>
-              <input
-                type="email"
-                id="email-address"
-                name="email"
-                autoComplete="email"
-                className="text-gray-900 border-gray-800 rounded block w-full p-3 placeholder-gray-500 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
-                value={email}
-                onChange={handleEmailChange}
-              />
+    <div>
+      <div className="flex flex-col items-center pt-8 pb-10 justify-center bg-red-100">
+        <div className="max-w-lg w-full p-10 bg-white rounded-xl shadow-lg">
+          <div className="flex flex-col items-center">
+            <img src="/images/Header/logo.png" alt="" className="w-auto h-12" />
+            <h2 className="mt-6 text-center text-2xl font-extrabold text-gray-900">
+              Create Your CNN Account
+            </h2>
+            <div className="text-sm mt-2">
+              <Link
+                to="/login"
+                className="font-medium text-black hover:text-gray-700"
+              >
+                Already have an account? <span>Sign In</span>
+              </Link>
             </div>
           </div>
-        </form>
+          <form className="mt-8" onSubmit={handleSubmit}>
+            <div className="rounded-md shadow-sm -space-y-px">
+              <div className="mb-4">
+                {/* <label htmlFor="email-address" className="sr-only">
+                Email Address
+              </label> */}
+                <input
+                  type="email"
+                  id="email-address"
+                  name="email"
+                  autoComplete="email"
+                  required
+                  className="text-gray-900 border border-gray-800 rounded block w-full p-3 placeholder-gray-500 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" // value={email}
+                  placeholder="Email Address"
+                  onChange={handleEmailChange}
+                />
+              </div>
+              <div className="mb-4 relative">
+                {/* <label htmlFor="email-address" className="sr-only">
+                Email Address
+              </label> */}
+                <input
+                  type="password"
+                  id="Password"
+                  name="password"
+                  autoComplete="password"
+                  required
+                  className="text-gray-900 border border-gray-800 rounded block w-full p-3 placeholder-gray-500 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" // value={email}
+                  placeholder="Password"
+                  onChange={handleEmailChange}
+                />
+                <span className="absolute right-3 top-1/2 transform -translate-y-1/2 cursor-pointer z-20">
+                  {showPassword ? <Eye size={20} /> : <EyeOff size={20} />}
+                </span>
+              </div>
+            </div>
+            <div className="flex items-center justify-between mt-5 mb-2">
+              <div className="flex text-sm items-center">
+                <p className="text-xs text-black-600 hover:text-black-500">
+                  By signing up or signing in, you agree to our Terms of Use and
+                  have read our Privacy Policy. CNN and its affiliates may use
+                  your email address to send updates, ads, and offers. Opt out
+                  via Privacy Policy.
+                </p>
+              </div>
+            </div>
+            <div>
+              <button
+                type="submit"
+                className="w-full flex justify-center py-3 px-4 border border-transparent text-md font-bold rounded-md text-white bg-red-600 hover:bg-red-700 focus:outline-none"
+              >
+                Create Account
+              </button>
+            </div>
+            <div className="flex items-center justify-between mt-2 mb-2">
+              <div className="flex text-sm items-center">
+                <p className="text-xs text-black-600 hover:text-black-500">
+                  To opt-out at any time see options available here
+                </p>
+              </div>
+            </div>
+          </form>
+        </div>
+        
       </div>
+      <AuthFooter />
     </div>
   );
 };
